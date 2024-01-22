902e7b9cee811b3035bfada3fdecc1d2760c05e3
Eric <eroija@gmail.com>
2024-01-22 12:58:08 +0300

    5

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    try:
-        raise TypeError("Custom type exception raised")
-    except TypeError as e:
-        print(f"Caught an exception: {e}")
+    raise TypeError
