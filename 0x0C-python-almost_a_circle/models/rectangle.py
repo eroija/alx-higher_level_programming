2d3af3feb1ba0a58d9754539e82c4edc2ea7bcd0
Eric <eroija@gmail.com>
2024-02-12 19:04:40 +0300

    2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,7 +39,7 @@ class Rectangle(Base):
         def width(self, value):
 
             """Setter for the width attribute."""
-            if not isinstance(value, int):
+            if type(value) is not int:
                 raise TypeError("width must be an integer")
             if value <= 0:
                 raise ValueError("width must be > 0")
