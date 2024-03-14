992a516e6c5c42d010f9c3412ba5ce9b2c882dd6
Eric <eroija@gmail.com>
2024-03-14 03:05:37 +0300

    00

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,5 @@
 -- creates database hbtn_0d_2 and the user user_0d_2
--- creates a database
+-- creates database
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 -- creates a user
 CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
