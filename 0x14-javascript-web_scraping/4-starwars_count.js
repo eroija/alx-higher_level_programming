2e8ba678ebaa35ddd65d0de6b3c5fc9e41a4d063
Eric <eroija@gmail.com>
2024-05-22 12:12:33 +0300

    4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,20 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
-
-const apiUrl = 'https://swapi-api.alx-tools.com/api/films/';
-
-const characterId = 18;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Error fetching data:', error);
-    return;
+request(process.argv[2], function (err, res, body) {
+  if (!err) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, film) => {
+      return film.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
-
-  const films = JSON.parse(body).results;
-  const moviesWithWedge = films.filter(film =>
-    film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-  );
-
-  console.log(`Number of movies with Wedge Antilles: ${moviesWithWedge.length}`);
 });
