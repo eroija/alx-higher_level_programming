7796d412b63b1b12db7943807f208c4514ebab65
Eric <eroija@gmail.com>
2024-05-22 12:21:43 +0300

    5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,27 +1,4 @@
 #!/usr/bin/node
 const fs = require('fs');
 const request = require('request');
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error('Error:', error);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error('API request failed with status code:', response.statusCode);
-    return;
-  }
-
-  fs.writeFile(filePath, body, 'utf-8', (err) => {
-    if (err) {
-      console.error('Error writing file:', err);
-      return;
-    }
-
-    console.log(`Content saved to file: ${filePath}`);
-  });
-});
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
