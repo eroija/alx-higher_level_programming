a91c586841e53f280c16c5ecb1045f300b39270d
Eric <eroija@gmail.com>
2024-05-24 04:32:58 +0300

    101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,23 +1,20 @@
-#!/usr/bin/node
+#a!/usr/bin/node
 const request = require('request');
-const movieId = process.argv[2];
-
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-request(url, { json: true }, (err, res, body) => {
-  if (err) {
-    return console.error(err);
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(url, function (err, res, body) {
+  if (!err) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
-  if (res.statusCode !== 200) {
-    return console.error(`Error: Status Code ${res.statusCode}`);
-  }
-
-  body.characters.forEach(characterUrl => {
-    request(characterUrl, { json: true }, (err, res, body) => {
-      if (err) {
-        return console.error(err);
-      }
-      console.log(body.name);
-    });
-  });
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (err, res, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
