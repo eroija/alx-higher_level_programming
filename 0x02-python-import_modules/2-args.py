4b81801734b934579c961023a28f7b0ba7289b36
Eric <eroija@gmail.com>
2024-12-15 16:36:40 +0300

    3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if num == 0:
         print("{} arguments.".format(num))
     elif num == 1:
-        print("{} argument: ".format(num))
+        print("{} argument:".format(num))
     else:
         print("{} arguments:".format(num))
 
