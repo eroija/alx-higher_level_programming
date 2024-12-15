b58d5ab846d26764b8a62530b4a3c60222c11950
Eric <eroija@gmail.com>
2024-12-15 16:34:27 +0300

    22

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,5 +11,5 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(num))
 
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+    for i in range(num):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
